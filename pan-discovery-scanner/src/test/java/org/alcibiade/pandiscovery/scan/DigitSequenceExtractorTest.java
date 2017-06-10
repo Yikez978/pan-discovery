8be2579e32c5dc68f46e2f58740ee6b349099d77
Yannick Kirschhoffer <alcibiade@alcibiade.org>
2017-06-10 21:34:37 +0200

    Add a DigitSequenceExtractor performance test.

--- pan-discovery-scanner/src/test/java/org/alcibiade/pandiscovery/scan/DigitSequenceExtractorTest.java
+++ pan-discovery-scanner/src/test/java/org/alcibiade/pandiscovery/scan/DigitSequenceExtractorTest.java
@@ -2,8 +2,16 @@ package org.alcibiade.pandiscovery.scan;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class DigitSequenceExtractorTest {
+
+    private Logger logger = LoggerFactory.getLogger(DigitSequenceExtractorTest.class);
 
     @Test
     public void testExtration() {
@@ -36,5 +44,34 @@ public class DigitSequenceExtractorTest {
         Assertions.assertThat(extractor.extractSequences("Hello")).isEmpty();
         Assertions.assertThat(extractor.extractSequences(
             "0123 4567 89012345")).hasSize(2).contains("4567");
+    }
+
+
+    @Test
+    public void testPerformance() {
+        List<String> inputSet = new ArrayList<>();
+        Random random = new Random();
+
+        while (inputSet.size() < 1000000) {
+            StringBuilder text = new StringBuilder();
+
+            while (text.length() < 128) {
+                text.append((char) ('0' + random.nextInt(10)));
+
+                if (random.nextInt(10) == 0) {
+                    text.append((char) ('a' + random.nextInt(26)));
+                }
+            }
+
+            inputSet.add(text.toString());
+        }
+
+        DigitSequenceExtractor extractor = new DigitSequenceExtractor(16);
+
+        long tsStart = System.currentTimeMillis();
+        inputSet.forEach(extractor::extractSequences);
+        long tsEnd = System.currentTimeMillis();
+
+        logger.debug("Extraction duration: {}ms", (tsEnd - tsStart));
     }
 }
